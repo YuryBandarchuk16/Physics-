a22575577fa3a106a75dbab2303e48349860d3dd
ViktoriaErokhina <viktoria.erokhina@gmail.com>
2016-11-21 16:44:19 +0300

    MainWindow created and some bugs fixed

--- src/sample/MainWindow.java
+++ src/sample/MainWindow.java
@@ -8,21 +8,19 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 /**
- * Created by YuryBandarchuk on 15.11.16.
+ * Created by Viktoria on 21.11.16.
  */
-
-public class Main extends Application {
-
+public class MainWindow extends Application {
     private static Stage primaryStage;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Main Menu");
+        Parent root = FXMLLoader.load(getClass().getResource("mainWindow.fxml"));
+        primaryStage.setTitle("Main Window");
         primaryStage.setScene(new Scene(root, 600, 400));
         primaryStage.show();
         primaryStage.setOnCloseRequest(e -> Platform.exit());
-        Main.primaryStage = primaryStage;
+        MainWindow.primaryStage = primaryStage;
     }
 
     public static void main(String[] args) {
@@ -31,17 +29,6 @@ public class Main extends Application {
 
     public static void close() {
         primaryStage.close();
-    }
-
-    public static void runDynamics() {
-        Platform.runLater(() -> {
-            try {
-                new DynamicsWindow().start(new Stage());
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        });
-        close();
     }
 
 }
---
+++ src/sample/MainWindowController.java
@@ -0,0 +1,8 @@
+package sample;
+
+/**
+ * Created by Viktoria on 21.11.16.
+ */
+
+public class MainWindowController {
+}
